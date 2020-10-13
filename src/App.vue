cf3cf3c3a19562eb7ed9cffc3e7fdc298e54964d
Mezbaul Islam Protick <mezbaulislamprotick@outlook.com>
2020-10-14 05:54:23 +0600

    Styling the form

--- src/App.vue
+++ src/App.vue
@@ -85,6 +85,73 @@ export default {
   margin-top: 60px;
 }
 
+form {
+  padding: 15px;
+  margin: 0 auto;
+}
 
+button, input {
+  border: 0;
+  outline: 0;
+  font-size: 16px;
+  border-radius: 40px;
+  padding: 12px;
+  background-color: #EBECF0;
+  text-shadow: 1px 1px 0 #FFF;
+}
+
+label {
+  display: block;
+  width: 100%;
+  margin: 10px 0 10px 0;
+}
+
+input {
+  box-shadow: inset 2px 2px 5px #BABECC, inset -5px -5px 10px #FFF;
+  width: 100%;
+  box-sizing: border-box;
+  transition: all 0.2s ease-in-out;
+  appearance: none;
+  -webkit-appearance: none;
+}
+input:focus {
+  box-shadow: inset 1px 1px 2px #BABECC, inset -1px -1px 2px #FFF;
+}
+
+button {
+  color: #61677C;
+  font-weight: bold;
+  box-shadow: -5px -5px 20px #FFF, 5px 5px 20px #BABECC;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 600;
+  margin: 10px 0;
+}
+button:hover {
+  box-shadow: -2px -2px 5px #FFF, 2px 2px 5px #BABECC;
+}
+button:active {
+  box-shadow: inset 1px 1px 2px #BABECC, inset -1px -1px 2px #FFF;
+}
+
+.container{
+  width: 60%;
+  margin: auto;
+}
+
+.form_section{
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+}
+
+.form_part{
+  display: inline;
+  padding: 0 5px;
+}
+
+#todo_desc{
+  width: 40%;
+}
 
 </style>
